de3e6785cafa886b25b2047b09a61be02ebde684
purofle <3272912942@qq.com>
2020-10-16 18:18:39 +0000

    smjb vnc

--- src/main/kotlin/com/github/purofle/TestMod.kt
+++ src/main/kotlin/com/github/purofle/TestMod.kt
@@ -3,24 +3,28 @@ package com.github.purofle
 import arc.Events
 import arc.util.Log
 import arc.util.Time
-import mindustry.Vars
-import mindustry.game.EventType
+import com.github.purofle.content.Blocks
 import mindustry.game.EventType.WorldLoadEvent
 import mindustry.mod.Mod
-import mindustry.ui.dialogs.BaseDialog
+import mindustry.ui.dialogs.*
+import mindustry.world.Block
+import mindustry.world.blocks.defense.turrets.ItemTurret
 
 public class TestMod : Mod() {
     init {
         Log.info("插件开始加载")
 
         Events.on(WorldLoadEvent::class.java) {
-            Time.runTask(20f) {
-                Vars.ui.showErrorMessage("nmsl")
+            Time.runTask(10f) {
+                val dialog = BaseDialog("欢迎使用本mod!")
+                dialog.cont.add("[red]本mod目前尚未完善（")
+                dialog.cont.button("给爷爬", dialog::hide).size(70f, 50f)
+                dialog.show()
             }
         }
     }
 
     override fun loadContent() {
-        Log.info("test")
+        Blocks().load()
     }
 }
---
+++ src/main/kotlin/com/github/purofle/content/Blocks.kt
@@ -0,0 +1,26 @@
+package com.github.purofle.content
+
+import com.github.purofle.world.blocks.environment.WallPanel
+import mindustry.content.Items
+import mindustry.ctype.ContentList
+import mindustry.type.Category
+import mindustry.type.ItemStack
+import mindustry.world.meta.BuildVisibility
+
+class Blocks : ContentList {
+
+    companion object {
+        var BigWall: WallPanel? = null
+    }
+
+    override fun load() {
+        BigWall = object : WallPanel("大墙") {
+            init {
+                requirements(Category.defense, ItemStack.with(Items.lead, 5, Items.silicon, 2))
+                isConductor = true
+                size = 1
+                buildVisibility = BuildVisibility.sandboxOnly
+            }
+        }
+    }
+}
