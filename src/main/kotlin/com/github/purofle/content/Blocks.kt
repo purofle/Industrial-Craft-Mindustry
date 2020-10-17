465c26fbc0a1d642c0ca3f62a1ca7c7e6aa331a0
purofle <3272912942@qq.com>
2020-10-17 01:49:44 +0000

    smjb vnc

--- src/main/kotlin/com/github/purofle/content/Blocks.kt
+++ src/main/kotlin/com/github/purofle/content/Blocks.kt
@@ -1,26 +1,38 @@
 package com.github.purofle.content
 
-import com.github.purofle.world.blocks.environment.WallPanel
+import mindustry.content.Bullets
+import mindustry.content.Fx
 import mindustry.content.Items
 import mindustry.ctype.ContentList
 import mindustry.type.Category
-import mindustry.type.ItemStack
-import mindustry.world.meta.BuildVisibility
+import mindustry.type.ItemStack.with
+import mindustry.world.blocks.defense.turrets.ItemTurret
 
 class Blocks : ContentList {
 
     companion object {
-        var BigWall: WallPanel? = null
+        var ST: ItemTurret? = null
     }
 
     override fun load() {
-        BigWall = object : WallPanel("大墙") {
+        ST = object : ItemTurret("st") {
             init {
-                requirements(Category.defense, ItemStack.with(Items.lead, 5, Items.silicon, 5))
-                isConductor = true
-                health = 100
-                size = 1
-                buildVisibility = BuildVisibility.sandboxOnly
+                requirements(Category.turret, with(Items.copper, 50), true)
+                ammo(
+                        Items.copper, Bullets.standardCopper
+                )
+
+                spread = 4f
+                shots = 2
+                alternate = true
+                reloadTime = 20f
+                restitution = 0.01f
+                range = 200f
+                shootCone = 20f
+                ammoUseEffect = Fx.shellEjectSmall
+                health = 200
+                inaccuracy = 2f
+                rotateSpeed = 10f
             }
         }
     }
