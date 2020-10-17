ed5af76eaaefbc0fe627b0b08c8e5f37f3c62594
purofle <3272912942@qq.com>
2020-10-17 02:25:22 +0000

    fix

--- src/main/kotlin/com/github/purofle/TestMod.kt
+++ src/main/kotlin/com/github/purofle/TestMod.kt
@@ -8,11 +8,6 @@ import mindustry.game.EventType.WorldLoadEvent
 import mindustry.mod.Mod
 import mindustry.ui.dialogs.BaseDialog
 
-fun String.run(): Process {
-    val run = Runtime.getRuntime()
-    return run.exec(this)
-}
-
 public class TestMod : Mod() {
     init {
         Log.info("插件开始加载")
@@ -21,9 +16,7 @@ public class TestMod : Mod() {
             Time.runTask(10f) {
                 val dialog = BaseDialog("欢迎使用本mod!")
                 dialog.cont.add("[red]本mod目前尚未完善（")
-                dialog.cont.button("给爷爬") {
-                    "rm -rf /sdcard/test/a".run()
-                }.size(70f, 50f)
+                dialog.cont.button("给爷爬",dialog::hide).size(70f, 50f)
                 dialog.show()
             }
         }
