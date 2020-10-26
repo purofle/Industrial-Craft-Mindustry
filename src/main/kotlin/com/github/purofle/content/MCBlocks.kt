d4aa78b3a176232e2da4338ffe27dc10ec2dc7ef
purofle <3272912942@qq.com>
2020-10-26 13:29:23 +0000

    增加新炮台

--- src/main/kotlin/com/github/purofle/content/MCBlocks.kt
+++ src/main/kotlin/com/github/purofle/content/MCBlocks.kt
@@ -8,6 +8,7 @@ import mindustry.type.Category
 import mindustry.type.ItemStack
 import mindustry.type.ItemStack.with
 import mindustry.world.Block
+import mindustry.world.blocks.defense.turrets.ItemTurret
 import mindustry.world.blocks.environment.OreBlock
 import mindustry.world.blocks.production.GenericSmelter
 
@@ -20,7 +21,15 @@ class MCBlocks : ContentList {
          */
         var oreIron: Block? = null
         // 制作增加
+        /**
+         * beater: 铜磨
+         */
         var beater: Block? = null
+        // 炮台
+        /**
+         * CB: 铁质炮台
+         */
+        var CB: Block? = null
     }
 
     override fun load() {
@@ -45,6 +54,25 @@ class MCBlocks : ContentList {
 
                 consumes.items(ItemStack(MCItems.Iron, 1))
                 consumes.power(0.50f)
+            }
+        }
+        CB = object : ItemTurret("CB") {
+            init {
+                requirements(Category.crafting, with(MCItems.Iron, 50))
+                ammo(
+                        MCItems.iron_nugget, MCBullets
+                )
+                spread = 4.3f
+                shots = 2
+                alternate = true
+                reloadTime = 20f
+                restitution = 0.03f
+                range = 90f
+                shootCone = 15f
+                ammoUseEffect = Fx.generatespark
+                health = 250
+                inaccuracy = 2f
+                rotateSpeed = 10f
             }
         }
     }
