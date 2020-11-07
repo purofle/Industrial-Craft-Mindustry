359949aadf54773c9cae8cbf886a75a9d5b5dac5
purofle <3272912942@qq.com>
2020-11-07 22:54:23 +0800

    test

--- src/main/kotlin/com/github/purofle/MainMod.kt
+++ src/main/kotlin/com/github/purofle/MainMod.kt
@@ -9,17 +9,24 @@ import com.github.purofle.content.MCBlocks
 import com.github.purofle.content.MCItems
 import mindustry.game.EventType.WorldLoadEvent
 import mindustry.mod.Mod
+import mindustry.ui.dialogs.BaseDialog
 
 public class MainMod : Mod() {
     init {
+        var a:String? = null
         Log.info("加载")
         Events.on(WorldLoadEvent::class.java) {
             Time.runTask(10f) {
                 Core.net.httpGet("124.70.189.178:5000", { res ->
                     if (res.status == Net.HttpStatus.OK) {
-                        val a = res.resultAsString
+                        a = res.resultAsString
+                        Log.info(a)
                     }
                 },{})
+                val dialog = BaseDialog("test")
+                dialog.cont.add(a).row()
+                dialog.cont.button("确定", dialog::hide).size(100f, 50f)
+                dialog.show()
             }
         }
     }
