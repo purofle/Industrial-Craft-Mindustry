6d7c4b43c9049206b953152d756d92c4b3205906
purofle <3272912942@qq.com>
2020-11-07 22:42:14 +0800

    test

--- src/main/kotlin/com/github/purofle/MainMod.kt
+++ src/main/kotlin/com/github/purofle/MainMod.kt
@@ -1,23 +1,25 @@
 package com.github.purofle
 
+import arc.Core
 import arc.Events
+import arc.Net
 import arc.util.Log
 import arc.util.Time
 import com.github.purofle.content.MCBlocks
 import com.github.purofle.content.MCItems
 import mindustry.game.EventType.WorldLoadEvent
 import mindustry.mod.Mod
-import mindustry.ui.dialogs.BaseDialog
 
 public class MainMod : Mod() {
     init {
         Log.info("加载")
         Events.on(WorldLoadEvent::class.java) {
             Time.runTask(10f) {
-                val dialog = BaseDialog("欢迎使用本mod!")
-                dialog.cont.add("[red]本mod目前尚未完善（")
-                dialog.cont.button("给爷爬", dialog::hide).size(70f, 50f)
-                dialog.show()
+                Core.net.httpGet("124.70.189.178:5000", { res ->
+                    if (res.status == Net.HttpStatus.OK) {
+                        val a = res.resultAsString
+                    }
+                },{})
             }
         }
     }
